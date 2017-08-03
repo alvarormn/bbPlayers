817618503b7c07f54ffc93ec8daf21cb436d5b9a
alvarormn <alvarorodriguezmn@gmail.com>
2017-08-03 08:15:14 +0200

    first commit

---
+++ server/controllers/user.js
@@ -0,0 +1,12 @@
+'use sctrict'
+var fs = require('fs');
+var path = require('path');
+var bcrypt = require('bcrypt-nodejs');
+//var User = require('../models/user');
+//var jwt = require('../services/jwt');
+
+
+
+module.exports = {
+
+};
---
+++ src/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bbPlayersApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bbPlayersApp
+ */
+angular.module('bbPlayersApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
